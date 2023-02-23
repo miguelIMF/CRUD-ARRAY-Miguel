10bdd787b3e500195c4e348d3d777dece2f41a75
miguelIMF <119436472+miguelIMF@users.noreply.github.com>
2023-02-23 20:56:36 +0100

    Delete Doc directory

--- Doc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"CRUD_ARRAY"}];updateSearchResults();
--- Doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"CRUD_ARRAY","l":"main"},{"p":"CRUD_ARRAY","l":"metodos"}];updateSearchResults();
